787c4d8093a1be99c68a4288aca1c5239c87bc46
Mustapha Moulana <mm@hygeos.com>
2024-12-18 16:25:53 +0100

    small correction and some improvement in test_basic

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,20 +10,24 @@ import sys
 sys.path.insert(0, ROOTPATH)
 import geoclide as gc
 
-P1 = [np.array([1.,2.,3.])]
+P1 = [np.array([0.,0.,0.]), np.array([1.,2.,3.])]
+V1 = [np.array([0.,0.,1.]), np.array([1.,1.,1.])]
+N1 = [np.array([0.,0.,1.]), np.array([1.,1.,1.])]
 
 
-def test_vector():
-    v1 = gc.Vector(0.,0.,1.)
-    v2 = gc.Vector(np.array([0.,0.,1.]))
+@pytest.mark.parametrize('v_arr', V1)
+def test_vector(v_arr):
+    v1 = gc.Vector(v_arr[0], v_arr[1], v_arr[2])
+    v2 = gc.Vector(v_arr)
     assert (v1 == v2)
-    assert (np.all(v1.to_numpy() == np.array([0.,0.,1.])))
-    assert (v1[0] == 0.)
-    assert (v1[1] == 0.)
-    assert (v1[2] == 1.)
+    assert (np.all(v1.to_numpy() == v_arr))
+    assert (v1[0] == v_arr[0])
+    assert (v1[1] == v_arr[1])
+    assert (v1[2] == v_arr[2])
     assert (v1[0] == v1.x)
     assert (v1[1] == v1.y)
     assert (v1[2] == v1.z)
+
 
 @pytest.mark.parametrize('p_arr', P1)
 def test_point(p_arr):
@@ -31,53 +35,57 @@ def test_point(p_arr):
     p2 = gc.Point(p_arr)
     assert (p1 == p2)
     assert (np.all(p1.to_numpy() == p_arr))
-    assert (p1[0] == 1.)
-    assert (p1[1] == 2.)
-    assert (p1[2] == 3.)
+    assert (p1[0] == p_arr[0])
+    assert (p1[1] == p_arr[1])
+    assert (p1[2] == p_arr[2])
     assert (p1[0] == p1.x)
     assert (p1[1] == p1.y)
     assert (p1[2] == p1.z)
 
 
-def test_normal():
-    n1 = gc.Normal(0.,0.,1.)
-    n2 = gc.Normal(np.array([0.,0.,1.]))
+@pytest.mark.parametrize('n_arr', N1)
+def test_normal(n_arr):
+    n1 = gc.Normal(n_arr[0], n_arr[1], n_arr[2])
+    n2 = gc.Normal(n_arr)
     assert (n1 == n2)
-    assert (np.all(n1.to_numpy() == np.array([0.,0.,1.])))
-    assert (n1[0] == 0.)
-    assert (n1[1] == 0.)
-    assert (n1[2] == 1.)
+    assert (np.all(n1.to_numpy() == n_arr))
+    assert (n1[0] == n_arr[0])
+    assert (n1[1] == n_arr[1])
+    assert (n1[2] == n_arr[2])
     assert (n1[0] == n1.x)
     assert (n1[1] == n1.y)
     assert (n1[2] == n1.z)
 
 
-def test_opeVector():
-    v1 = gc.Vector(0., 0., 1.)
-    v2 = gc.Vector(1., 0., 0.)
-    assert (v1+v2 == gc.Vector(1., 0., 1.))
-    assert (v1-v2 == gc.Vector(-1., 0., 1.))
-    assert (v1*2 == gc.Vector(0., 0., 2.))
-    assert (v1/2 == gc.Vector(0., 0., 0.5))
+@pytest.mark.parametrize('v_arr', V1)
+def test_opeVector(v_arr):
+    v1 = gc.Vector(v_arr)
+    v2 = gc.Vector(1., 3., 0.5)
+    assert (v1+v2 == gc.Vector(v_arr+v2.to_numpy()))
+    assert (v1-v2 == gc.Vector(v_arr-v2.to_numpy()))
+    assert (v1*2 == gc.Vector(v_arr*2.))
+    assert (v1/2 == gc.Vector(v_arr/2.))
     v3 = v1+v2
     assert (v3.length() == math.sqrt(v3[0]**2+v3[1]**2+v3[2]**2))
 
 
-def test_opePoint():
-    p1 = gc.Point(1.,2.,3.)
-    v1 = gc.Vector(0., 0., 1.)
+@pytest.mark.parametrize('p_arr', P1)
+def test_opePoint(p_arr):
+    p1 = gc.Point(p_arr)
+    v1 = gc.Vector(1., 3., 0.5)
     p2 = gc.Point(1.,1.,1.)
-    assert (p1+v1 == gc.Point(1., 2., 4.))
-    assert (p1-p2 == gc.Vector(0.0, 1.0, 2.0))
-    assert (p1-v1 == gc.Point(1.0, 2.0, 2.0))
-    assert (p1*2 == gc.Point(2.0, 4.0, 6.0))
-    assert (p1/2 == gc.Point(0.5, 1.0, 1.5))
+    assert (p1+v1 == gc.Point(p_arr+v1.to_numpy()))
+    assert (p1-p2 == gc.Vector(p_arr-p2.to_numpy()))
+    assert (p1-v1 == gc.Point(p_arr-v1.to_numpy()))
+    assert (p1*2 == gc.Point(p_arr*2.))
+    assert (p1/2 == gc.Point(p_arr/2.))
 
 
-def test_opeNormal():
-    n1 = gc.Normal(0.,0.,1.)
-    n2 = gc.Normal(1., 0., 0.)
-    assert (n1+n2 == gc.Normal(1.0, 0.0, 1.0))
-    assert (n1-n2 == gc.Normal(-1.0, 0.0, 1.0))
-    assert (n1*2 == gc.Normal(0.0, 0.0, 2.0))
-    assert (n1/2 == gc.Normal(0.0, 0.0, 0.5))
+@pytest.mark.parametrize('n_arr', N1)
+def test_opeNormal(n_arr):
+    n1 = gc.Normal(n_arr)
+    n2 = gc.Normal(1., 3., 0.5)
+    assert (n1+n2 == gc.Normal(n_arr+n2.to_numpy()))
+    assert (n1-n2 == gc.Normal(n_arr-n2.to_numpy()))
+    assert (n1*2 == gc.Normal(n_arr*2.))
+    assert (n1/2 == gc.Normal(n_arr/2.))
