4a6550f0f7893387084ff41dc3369e75699aee9d
Mustapha Moulana <mm@hygeos.com>
2025-01-09 13:03:09 +0100

    move mathope tests to the correct file

--- tests/test_vecope.py
+++ tests/test_vecope.py
@@ -116,19 +116,3 @@ def test_permute():
     assert (gc.permute(v1, np.array([1, 0, 2])) == gc.Vector(3.0, 2.0, 1.0))
 
 
-def test_clamp():
-    assert (gc.clamp(4., val_min=5., val_max=11.) == 5.)
-
-
-def test_swap():
-    a = 5.
-    b = 12.
-    assert (gc.swap(a, b) == (b, a))
-
-
-def test_quadratic():
-    a = 2.
-    b = -5.
-    c = 0.
-    assert (gc.quadratic(a, b, c) == (True, 0.0, 2.5))
-
