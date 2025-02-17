4dc04dae6a05c8c4c7b34d11102719d74c6b14a3
Mustapha Moulana <mm@hygeos.com>
2025-02-17 16:35:38 +0100

    Correction in test_trianglemesh.py

--- geoclide/tests/test_trianglemesh.py
+++ geoclide/tests/test_trianglemesh.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import geoclide as gc
 import numpy as np
 import math
+import geoclide as gc
+import os
+
+ROOTPATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 
 def test_triangle_intersection():
@@ -148,22 +151,22 @@ def test_triangle_mesh():
 
 
 def test_read_gcnc_trianglemesh():
-    msh_read = gc.read_gcnc_trianglemesh('geoclide/tests/data/sphere_r1p5_resth18_resph36.gcnc')
+    msh_read = gc.read_gcnc_trianglemesh(ROOTPATH + '/geoclide/tests/data/sphere_r1p5_resth18_resph36.gcnc')
     msh = gc.Sphere(1.5).to_trianglemesh(reso_theta=18, reso_phi=36)
     assert (np.all(np.isclose(msh_read.vertices, msh.vertices, 0., 1e-15)))
     assert (np.all(msh_read.faces == msh.faces))
 
-    msh_read = gc.read_gcnc_trianglemesh('geoclide/tests/data/sphere_r1p5_resth18_resph18_phimax180.gcnc')
+    msh_read = gc.read_gcnc_trianglemesh(ROOTPATH + '/geoclide/tests/data/sphere_r1p5_resth18_resph18_phimax180.gcnc')
     msh = gc.Sphere(1.5, phi_max=180.).to_trianglemesh(reso_theta=18, reso_phi=18)
     assert (np.all(np.isclose(msh_read.vertices, msh.vertices, 0., 1e-15)))
     assert (np.all(msh_read.faces == msh.faces))
 
-    msh_read = gc.read_gcnc_trianglemesh('geoclide/tests/data/oblate_rxy1p5_rz1p2_resth18_resph36.gcnc')
+    msh_read = gc.read_gcnc_trianglemesh(ROOTPATH + '/geoclide/tests/data/oblate_rxy1p5_rz1p2_resth18_resph36.gcnc')
     msh = gc.Spheroid(1.5, 1.2).to_trianglemesh(reso_theta=18, reso_phi=36)
     assert (np.all(np.isclose(msh_read.vertices, msh.vertices, 0., 1e-15)))
     assert (np.all(msh_read.faces == msh.faces))
 
-    msh_read = gc.read_gcnc_trianglemesh('geoclide/tests/data/prolate_rxy1p5_rz3_resth18_resph36.gcnc')
+    msh_read = gc.read_gcnc_trianglemesh(ROOTPATH + '/geoclide/tests/data/prolate_rxy1p5_rz3_resth18_resph36.gcnc')
     msh = gc.Spheroid(1.5, 3.).to_trianglemesh(reso_theta=18, reso_phi=36)
     assert (np.all(np.isclose(msh_read.vertices, msh.vertices, 0., 1e-15)))
     assert (np.all(msh_read.faces == msh.faces))
