d6dbcf18dfa31fd088863383900de1ace3df476b
Mustapha Moulana <mm@hygeos.com>
2025-03-02 16:00:29 +0100

    Add more quadric tests

--- geoclide/tests/test_quadrics.py
+++ geoclide/tests/test_quadrics.py
@@ -3,6 +3,12 @@
 
 import numpy as np
 import geoclide as gc
+import pytest
+
+Q1 = [gc.Disk(radius=3., inner_radius=1.2),
+      gc.Sphere(radius=3.),
+      gc.Spheroid(radius_xy=3., radius_z=2.),
+      gc.Spheroid(radius_xy=2., radius_z=3.)]
 
 
 def test_sphere():
@@ -117,4 +123,67 @@ def test_disk():
     assert (ds_annulus['is_intersection'].values == annulus.is_intersection(r3))
 
 
+@pytest.mark.parametrize('quadric', Q1)
+def test_quadric_1d_arr(quadric):
+    x_, y_, z_ = np.meshgrid(np.linspace(-1.5, 1.5, 10, np.float64),
+                         np.linspace(-1.5, 1.5, 10, np.float64),
+                         5., indexing='ij')
+    o_set_arr = np.vstack((x_.ravel(), y_.ravel(), z_.ravel())).T
+    nrays = o_set_arr.shape[0]
+    d_set_arr = np.zeros_like(o_set_arr)
+    d_set_arr[:,0] = 0.
+    d_set_arr[:,1] = 0.
+    d_set_arr[:,2] = -1.
+    o_set = gc.Point(o_set_arr)
+    d_set = gc.Vector(d_set_arr)
+    r_set = gc.Ray(o_set, d_set)
 
+    ds = quadric.intersect(r_set, ds_output=True)
+
+    is_int_1d = np.full((nrays), True, dtype=bool)
+    t_1d = np.zeros((nrays), dtype=np.float64)
+    p_1d = np.zeros((nrays,3), dtype=np.float64)
+    n_1d = np.zeros_like(p_1d)
+    dpdu_1d = np.zeros_like(p_1d)
+    dpdv_1d = np.zeros_like(p_1d)
+    u_1d = np.zeros_like(t_1d)
+    v_1d = np.zeros_like(t_1d)
+    list_rays = []
+    for ir in range (0, nrays):
+        list_rays.append(gc.Ray(gc.Point(o_set_arr[ir,:]), gc.Vector(d_set_arr[ir,:])))
+
+    for ir in range (0, nrays):
+        res_sca = quadric.intersect(list_rays[ir], ds_output=False)
+        if (res_sca[2] is not None):
+            ds_sca = gc.get_intersect_dataset(*res_sca)
+            is_int_1d[ir] = ds_sca['is_intersection'].values
+            t_1d[ir] = ds_sca['thit'].values
+            p_1d[ir,:] = ds_sca['phit'].values
+            n_1d[ir,:] = ds_sca['nhit'].values
+            dpdu_1d[ir,:] = ds_sca['dpdu'].values
+            dpdv_1d[ir,:] = ds_sca['dpdv'].values
+            u_1d[ir] = ds_sca['u'].values
+            v_1d[ir] = ds_sca['v'].values
+        else:
+            is_int_1d[ir] = False
+            t_1d[ir] = None
+            p_1d[ir,:] = None
+            n_1d[ir,:] = None
+            dpdu_1d[ir,:] = None
+            dpdv_1d[ir,:] = None
+            u_1d[ir] = None
+            v_1d[ir] = None
+    
+    assert (np.array_equal(ds['is_intersection'].values, is_int_1d, equal_nan=True))
+    assert (np.array_equal(ds['thit'].values, t_1d, equal_nan=True))
+    assert (np.array_equal(ds['phit'].values, p_1d, equal_nan=True))
+    assert (np.allclose(ds['nhit'].values, n_1d, 0., 1e-15, equal_nan=True))
+    assert (np.allclose(ds['u'].values, u_1d, 0., 1e-15, equal_nan=True))
+    assert (np.allclose(ds['v'].values, v_1d, 0., 1e-15, equal_nan=True))
+    assert (np.allclose(ds['dpdu'].values, dpdu_1d, 0., 1e-15, equal_nan=True))
+    assert (np.allclose(ds['dpdv'].values, dpdv_1d, 0., 1e-14, equal_nan=True))
+
+    thit, is_int = quadric.is_intersection_t(r_set)
+
+    assert (np.array_equal(ds['is_intersection'].values, is_int, equal_nan=True))
+    assert (np.array_equal(ds['thit'].values, thit, equal_nan=True))
