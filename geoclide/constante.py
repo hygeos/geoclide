7e142b1e1c244b75440d5f143e79880d0d3b4645
Mustapha Moulana <mm@hygeos.com>
2026-08-03 17:18:45 +0200

    Modernize constante.py

--- geoclide/constante.py
+++ geoclide/constante.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from geoclide.mathope import gamma_f32, gamma_f64
 import math
 
-VERSION = '3.0.3'
+from geoclide.mathope import gamma_f32, gamma_f64
+
+VERSION = "3.0.3"
 
 GAMMA2_F32 = gamma_f32(2)
 GAMMA3_F32 = gamma_f32(3)
@@ -14,4 +12,4 @@ GAMMA2_F64 = gamma_f64(2)
 GAMMA3_F64 = gamma_f64(3)
 GAMMA5_F64 = gamma_f64(5)
 
-TWO_PI = math.pi*2.
+TWO_PI = math.pi * 2.0
