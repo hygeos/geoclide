44e0a2f3c45a9011e3f053de4eb488ab594779f0
Mustapha Moulana <mm@hygeos.com>
2026-08-03 18:50:52 +0200

    Modernize __init__.py

--- geoclide/__init__.py
+++ geoclide/__init__.py
@@ -1,12 +1,107 @@
-from geoclide.basic import Vector, Point, Normal, Ray, BBox, get_common_vertices, get_common_face
-from geoclide.vecope import dot, cross, normalize, coordinate_system, distance, face_forward, \
-    vmax, vmin, vargmax, vargmin, vabs, permute
-from geoclide.mathope import clamp, quadratic, gamma_f32, gamma_f64
-from geoclide.transform import Transform, get_translate_tf, get_scale_tf, \
-    get_rotate_x_tf, get_rotate_y_tf, get_rotate_z_tf, get_rotate_tf, get_inverse_tf
-from geoclide.quadrics import Sphere, Spheroid, Disk
-from geoclide.intersection import calc_intersection
-from geoclide.trianglemesh import Triangle, TriangleMesh, create_sphere_trianglemesh, \
-    read_trianglemesh, create_disk_trianglemesh
-from geoclide.constante import VERSION, GAMMA2_F32, GAMMA2_F64, GAMMA3_F32, GAMMA3_F64, GAMMA5_F32, GAMMA5_F64, TWO_PI
 from geoclide.advancedvecope import ang2vec, vec2ang
+from geoclide.basic import (
+    BBox,
+    Normal,
+    Point,
+    Ray,
+    Vector,
+    get_common_face,
+    get_common_vertices,
+)
+from geoclide.constante import (
+    GAMMA2_F32,
+    GAMMA2_F64,
+    GAMMA3_F32,
+    GAMMA3_F64,
+    GAMMA5_F32,
+    GAMMA5_F64,
+    TWO_PI,
+    VERSION,
+)
+from geoclide.intersection import calc_intersection
+from geoclide.mathope import clamp, gamma_f32, gamma_f64, quadratic
+from geoclide.quadrics import Disk, Sphere, Spheroid
+from geoclide.transform import (
+    Transform,
+    get_inverse_tf,
+    get_rotate_tf,
+    get_rotate_x_tf,
+    get_rotate_y_tf,
+    get_rotate_z_tf,
+    get_scale_tf,
+    get_translate_tf,
+)
+from geoclide.trianglemesh import (
+    Triangle,
+    TriangleMesh,
+    create_disk_trianglemesh,
+    create_sphere_trianglemesh,
+    read_trianglemesh,
+)
+from geoclide.vecope import (
+    coordinate_system,
+    cross,
+    distance,
+    dot,
+    face_forward,
+    normalize,
+    permute,
+    vabs,
+    vargmax,
+    vargmin,
+    vmax,
+    vmin,
+)
+
+__all__ = [
+    "GAMMA2_F32",
+    "GAMMA2_F64",
+    "GAMMA3_F32",
+    "GAMMA3_F64",
+    "GAMMA5_F32",
+    "GAMMA5_F64",
+    "TWO_PI",
+    "VERSION",
+    "BBox",
+    "Disk",
+    "Normal",
+    "Point",
+    "Ray",
+    "Sphere",
+    "Spheroid",
+    "Transform",
+    "Triangle",
+    "TriangleMesh",
+    "Vector",
+    "ang2vec",
+    "calc_intersection",
+    "clamp",
+    "coordinate_system",
+    "create_disk_trianglemesh",
+    "create_sphere_trianglemesh",
+    "cross",
+    "distance",
+    "dot",
+    "face_forward",
+    "gamma_f32",
+    "gamma_f64",
+    "get_common_face",
+    "get_common_vertices",
+    "get_inverse_tf",
+    "get_rotate_tf",
+    "get_rotate_x_tf",
+    "get_rotate_y_tf",
+    "get_rotate_z_tf",
+    "get_scale_tf",
+    "get_translate_tf",
+    "normalize",
+    "permute",
+    "quadratic",
+    "read_trianglemesh",
+    "vabs",
+    "vargmax",
+    "vargmin",
+    "vec2ang",
+    "vmax",
+    "vmin",
+]
